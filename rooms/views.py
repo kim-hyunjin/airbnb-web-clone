758566691eeaa19bb2c0e113cce5e6eaaed00359
kim-hyunjin <hyunjin1612@gmail.com>
2020-01-20 14:51:23 +0900

    use class based view

--- rooms/views.py
+++ rooms/views.py
@@ -1,13 +1,30 @@
-from math import ceil
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.utils import timezone
+from django.views.generic import ListView
 from . import models
 
+class HomeView(ListView):
+  """ HomeView Definition """
+  model = models.Room
+  paginate_by = 10
+  paginate_orphans = 5
+  ordering = "created"
+  context_object_name = "rooms"
 
+"""
+  def get_context_data(self, **kwargs):
+      context = super().get_context_data(**kwargs)
+      now = timezone.now()
+      context["now"] = now
+      return context
+      """
+
+"""
 def all_rooms(request):
-    page = request.GET.get("page")
+    page = request.GET.get("page", 1)
     room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10)
-    rooms = paginator.get_page(page)
-    print(vars(rooms.paginator))
-    return render(request, "rooms/home.html", {"rooms": rooms})
+    paginator = Paginator(room_list, 10, orphans=5)
+    try:
+      rooms = paginator.page(int(page))
+      return render(request, "rooms/home.html", {"page": rooms})
+    except EmptyPage:
+      return redirect("/")"""
