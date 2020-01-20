18b19b2e4fece12e6fcfa7ef19e93d26956ff618
kim-hyunjin <hyunjin1612@gmail.com>
2020-01-20 16:46:48 +0900

    detail view with class based view

--- rooms/views.py
+++ rooms/views.py
@@ -2,7 +2,7 @@ from django.utils import timezone
 from django.http import Http404
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from . import models
 
 class HomeView(ListView):
@@ -13,9 +13,17 @@ class HomeView(ListView):
     ordering = "created"
     context_object_name = "rooms"
 
+class RoomDetail(DetailView):
+    """ RoomDetail Definition """
+    model = models.Room
+    
+
+
+"""
 def room_detail(request, pk):
     try:
         room = models.Room.objects.get(pk=pk)
         return render(request, "rooms/detail.html", {'room': room})
     except models.Room.DoesNotExist:
         raise Http404()
+        """
