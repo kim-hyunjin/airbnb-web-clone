be5e66e5d78f24e0a286e69be47d34921fbcc969
kim-hyunjin <hyunjin1612@gmail.com>
2020-01-20 16:36:50 +0900

    detail view with function based view

--- rooms/models.py
+++ rooms/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django_countries.fields import CountryField
 from core import models as core_models
 
@@ -78,6 +79,9 @@ class Room(core_models.TimeStampedModel):
     def save(self, *args, **kwargs):
         self.city = str.capitalize(self.city)
         super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse("rooms:detail", kwargs={'pk': self.pk})
 
 
 class Photo(core_models.TimeStampedModel):
--- rooms/views.py
+++ rooms/views.py
@@ -1,30 +1,21 @@
 from django.utils import timezone
+from django.http import Http404
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views.generic import ListView
 from . import models
 
 class HomeView(ListView):
-  """ HomeView Definition """
-  model = models.Room
-  paginate_by = 10
-  paginate_orphans = 5
-  ordering = "created"
-  context_object_name = "rooms"
+    """ HomeView Definition """
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
 
-"""
-  def get_context_data(self, **kwargs):
-      context = super().get_context_data(**kwargs)
-      now = timezone.now()
-      context["now"] = now
-      return context
-      """
-
-"""
-def all_rooms(request):
-    page = request.GET.get("page", 1)
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10, orphans=5)
+def room_detail(request, pk):
     try:
-      rooms = paginator.page(int(page))
-      return render(request, "rooms/home.html", {"page": rooms})
-    except EmptyPage:
-      return redirect("/")"""
+        room = models.Room.objects.get(pk=pk)
+        return render(request, "rooms/detail.html", {'room': room})
+    except models.Room.DoesNotExist:
+        raise Http404()
