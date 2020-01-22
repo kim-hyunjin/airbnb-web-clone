b2e0316a6b64030e252b9c1bd35fec9bdc373241
kim-hyunjin <hyunjin1612@gmail.com>
2020-01-22 18:04:08 +0900

    search-bar

--- rooms/views.py
+++ rooms/views.py
@@ -1,29 +1,41 @@
-from django.utils import timezone
-from django.http import Http404
-from django.shortcuts import render, redirect
-from django.urls import reverse
+from django_countries import countries
+from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from . import models
 
+
 class HomeView(ListView):
     """ HomeView Definition """
+
     model = models.Room
     paginate_by = 10
     paginate_orphans = 5
     ordering = "created"
     context_object_name = "rooms"
 
+
 class RoomDetail(DetailView):
     """ RoomDetail Definition """
+
     model = models.Room
-    
 
 
-"""
-def room_detail(request, pk):
-    try:
-        room = models.Room.objects.get(pk=pk)
-        return render(request, "rooms/detail.html", {'room': room})
-    except models.Room.DoesNotExist:
-        raise Http404()
-        """
+def search(request):
+    city = request.GET.get("city", "Anywhere")
+    city = str.capitalize(city)
+    country = request.GET.get("country", "KR")
+    room_type = int(request.GET.get("room_type", 0))
+    room_types = models.RoomType.objects.all()
+
+    form = {
+        "city": city,
+        "s_room_type": room_type,
+        "s_country": country,
+    }
+
+    choices = {
+        "countries": countries,
+        "room_types": room_types,
+    }
+
+    return render(request, "rooms/search.html", {**form, **choices},)
