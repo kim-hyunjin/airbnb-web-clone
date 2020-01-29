e09c220176335518f713eb610e19b2cc2d385c2b
kim-hyunjin <hyunjin1612@gmail.com>
2020-01-29 17:32:20 +0900

    sign up

--- users/views.py
+++ users/views.py
@@ -5,10 +5,12 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
 from . import forms
 
+
 class LoginView(FormView):
     template_name = "users/login.html"
     form_class = forms.LoginForm
     success_url = reverse_lazy("core:home")
+
     def form_valid(self, form):
         email = form.cleaned_data.get("email")
         password = form.cleaned_data.get("password")
@@ -17,10 +19,23 @@ class LoginView(FormView):
             login(self.request, user)
         return super().form_valid(form)
 
+
 def log_out(request):
     logout(request)
     return redirect(reverse("core:home"))
 
+
 class SignUpView(FormView):
-    pass
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+
+    def form_valid(self, form):
+        form.save()
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
 
