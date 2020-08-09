fde971a4511d12765fe1ce38c383cfcedebb37dc
kim-hyunjin <hyunjin1612@gmail.com>
2020-08-09 19:50:41 +0900

    review, validator

--- reservations/views.py
+++ reservations/views.py
@@ -4,6 +4,7 @@ from django.views.generic import View
 from django.contrib import messages
 from django.shortcuts import render, redirect, reverse
 from rooms import models as room_models
+from reviews import forms as review_forms
 from . import models
 
 class CreateError(Exception):
@@ -35,7 +36,8 @@ class ReservationDetailView(View):
         reservation = models.Reservation.objects.get_or_none(pk=pk)
         if not reservation or (reservation.guest != self.request.user and reservation.room.host != self.request.user):
             raise Http404()
-        return render(self.request, "reservations/detail.html", {"reservation": reservation})
+        form = review_forms.CreateReviewForm()
+        return render(self.request, "reservations/detail.html", {"reservation": reservation, "form": form},)
 
 def edit_reservation(request, pk, verb):
     reservation = models.Reservation.objects.get_or_none(pk=pk)
