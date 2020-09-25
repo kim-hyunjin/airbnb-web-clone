b0bb79cba3f662d9350632e372e3f2427d2a2e2c
kim-hyunjin <hyunjin1612@gmail.com>
2020-09-25 23:58:42 +0900

    css 변경

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
       spacing: {
         "25vh": "25vh",
         "50vh": "50vh",
-        "75vh": "75vh"
+        "75vh": "75vh",
+        sm: '8px',
+        md: '16px',
+        lg: '24px',
+        xl: '48px',
       },
       borderRadius: {
         xl: "1.5rem"
@@ -12,6 +16,19 @@ module.exports = {
       minHeight: {
         "50vh": "50vh",
         "75vh": "75vh"
+      },
+      screens: {
+        'sm': '300px',
+        // => @media (min-width: 300px) { ... }
+  
+        'md': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
       }
     }
   },
