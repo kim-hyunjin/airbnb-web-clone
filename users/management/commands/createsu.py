32507f38e0c2482c68fbffb4a696b34834c8fafe
kim-hyunjin <hyunjin1612@gmail.com>
2020-09-25 16:22:35 +0900

    create superuser

--- users/management/commands/createsu.py
+++ users/management/commands/createsu.py
@@ -7,9 +7,9 @@ class Command(BaseCommand):
     help = "This command creates superuser"
 
     def handle(self, *args, **options):
-        admin = User.objects.get_or_none(username="ebadmin")
+        admin = User.objects.get_or_none(username="admin")
         if not admin:
-            User.objects.create_superuser("ebadmin", "admin@admin", "123456")
+            User.objects.create_superuser("admin", "admin@admin.com", "123456")
             self.stdout.write(self.style.SUCCESS(f"Superuser Created"))
         else:
             self.stdout.write(self.style.SUCCESS(f"Superuser Exists"))
